124e398077db493d0b6bb158f1da489eb5f7a080
Mirel <mireliovana@gmail.com>
2022-02-03 20:49:29 +0100

    tic tac toe basic function, updated design

--- projects/TicTacToe2/script.js
+++ projects/TicTacToe2/script.js
@@ -1,9 +1,3 @@
-var gameBoard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-];
-
 var winningValues = [
     ['0-0', '1-1', '2-2'],
     ['0-2', '1-1', '2-0'],
@@ -20,6 +14,10 @@ var winningValues = [
 var currentPlayer = "X"; //current player (X or O) - X starts
 var nextPlayer = "O";
 var gameOver = false;
+var xWon = false;
+var oWon = false;
+var draw = false;
+var totalClickedCells = 0;
 
 //create cells
 for (var row = 0; row < 3; row++) {
@@ -45,53 +43,42 @@ function clickHandler() {
     if(!gameOver) {
         //is cell clickable (has not been clicked before) ?
         if (clickedCell.classList.contains('clickable')) {
-            //update cell
-            (row + '-' + col);
-    
+            //update cell click tracker
+            totalClickedCells++;
+
             //register current player's move into cell
             clickedCell.classList.add(currentPlayer);
-            clickedCell.innerHTML = currentPlayer;
             
             //update board
-            var img = document.createElement('img');
-            if(currentPlayer === "X") {
-                img.src = 'img/x.png';
-            } else {
-                img.src = 'img/o.png';
-            }
-            img.classList.add('xo');
-            clickedCell.appendChild(img);
+            updateBoard(clickedCell);
             
             //check if any player won
-            var parent = Array.from(document.getElementsByClassName("X"));
-            const ids = parent.map(element => {
-                return element.id
-            });
-                //console.log(ids);
-            for(var i = 0; i < winningValues.length; i++){
-                var winner = 0;
-                ids.forEach(element => {
-                    for (var j = 0; j < 3; j++) {
-                        if(element === winningValues[i][j]) {
-                            //console.log('same' + element + 'winVal' + winningValues[i][j]);
-                            winner++;
-                        }
-                        console.log(winner);
-                    }
-                    
-                })
-                if(winner === 3) {
-                    gameOver = true;
-                    break;
-                }
-            };
-            if(gameOver) {
-                console.log("Game over");
+            var xCollection = Array.from(document.getElementsByClassName("X"));
+            var oCollection = Array.from(document.getElementsByClassName("O"));
+            if(xCollection.length > 2) {
+                checkWinner(xCollection, oCollection);
             }
-    
+
             //change current player into the other player
             checkPlayerTurn(currentPlayer); //check which player is next
-            currentPlayer = nextPlayer; //change player
+            const paragraph = document.getElementById("win-announcement");
+            paragraph.innerHTML = nextPlayer + '\'s turn';
+            currentPlayer = nextPlayer; //change player for next round
+
+            //do this if game over
+            if(gameOver) {
+                console.log("Game over");
+                const paragraph = document.getElementById("win-announcement");
+                if(xWon) {
+                    paragraph.innerHTML = "Winner: X";
+                } else if (oWon) {
+                    paragraph.innerHTML = "Winner: O";
+                } else if (draw) {
+                    paragraph.innerHTML = "It's a draw!";
+                }
+            }
+    
+            
     
             //make cell unclickable
             clickedCell.classList.remove('clickable');
@@ -105,11 +92,86 @@ function clickHandler() {
             console.log('cell has already been clicked');
         }
     } else {
-        const paragraph = document.createElement("p");
-        paragraph.innerHTML= "Someone won!";
-        document.getElementById('main-container').appendChild(paragraph);
+        //game over, what to do here?
 
     }
+}
+
+function updateBoard(clickedCell) {
+    var img = document.createElement('img');
+            if(currentPlayer === "X") {
+                img.src = 'img/x.png';
+                img.classList.add('x');
+            } else {
+                img.src = 'img/o.png';
+                img.classList.add('o');
+            }
+            img.classList.add('xo');
+            clickedCell.appendChild(img);
+}
+
+function makeCellsUnclickable() {
+    var cellCollection = Array.from(document.getElementsByClassName("clickable"));
+        cellCollection.forEach(element => { 
+            document.getElementById(element.id).classList.remove("clickable");
+        });
+}
+
+function checkWinner(xCollection, oCollection) {
+    //Did X win?
+    const xIds = xCollection.map(element => {
+        return element.id
+    });
+
+    for(var i = 0; i < winningValues.length; i++){
+        var xWinner = 0;
+        xIds.forEach(element => {
+            for (var j = 0; j < 3; j++) {
+                if(element === winningValues[i][j]) { // checks each x id against the winning values (by row)
+                    xWinner++; //counts how many winning values the x has from that row inside winningvalues
+                }
+            }
+            
+        })
+        if(xWinner === 3) {
+            xWon = true;
+            gameOver = true;
+            makeCellsUnclickable();
+            break;
+        }
+    };
+
+    //Did O win?
+    const oIds = oCollection.map(element => {
+        return element.id
+    });
+
+    for(var i = 0; i < winningValues.length; i++){
+        var oWinner = 0;
+        oIds.forEach(element => {
+            for (var j = 0; j < 3; j++) {
+                if(element === winningValues[i][j]) { // checks each o id against the winning values (by row)
+                    oWinner++; //counts how many winning values the o has from that row inside winningvalues
+                }
+            }
+            
+        })
+        if(oWinner === 3) {
+            oWon = true;
+            gameOver = true;
+            makeCellsUnclickable();
+            break;
+        }
+    };
+
+    //Draw?
+    if (totalClickedCells === 9 && !xWon && !oWon) {
+        draw = true;
+        gameOver = true;
+        makeCellsUnclickable();
+    }
+    
+
 }
 
 function checkPlayerTurn(currentPlayer) {
